6e41f43d24e4df563a479f31a9709d6f8d86f361
ROHITH704 <nrohith704@gmail.com>
2025-07-15 09:40:19 +0530

    initial port

---
+++ main.js
@@ -0,0 +1,10 @@
+const colors = ['red', 'green', 'blue'];
+
+colors.forEach((color, idx) => {
+    console.log(`${idx}: ${color}`);
+});
+
+for (let i = 0; i < colors.length; i++) {
+    console.log(`Color at ${i} is ${colors[i]}`);
+}
+
